30c605b76adbff160c9fc4ae53180760d5acc4e0
arman asl yosef zadeh <78927511+armanyosefzadeh@users.noreply.github.com>
2021-07-14 12:33:20 +0430

    Update lr_utils.py

--- project 1/lr_utils.py
+++ project 1/lr_utils.py
@@ -10,11 +10,11 @@ import h5py
     
     
 def load_dataset():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_dataset = h5py.File('https://drive.google.com/file/d/1rA_FRXc_MwS1uGBaBJ-mqMXPvirhr3PP/view?usp=sharing')
     train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
     train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
 
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_dataset = h5py.File('https://drive.google.com/file/d/1Y5ixdMYl2bMia6eQ6zoNG8m2hW5Nhhcb/view?usp=sharing')
     test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
     test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
 
